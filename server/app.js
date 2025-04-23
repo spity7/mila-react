0d8c447277a6eea242f11d31cd6f7b9c11ee84b4
hasan sbeity <th.rider.clan@gmail.com>
2025-04-23 18:49:56 +0300

    s

--- server/app.js
+++ server/app.js
@@ -41,7 +41,7 @@ app.use("/contact-us", contactUsRouter);
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 const start = async () => {
   try {
