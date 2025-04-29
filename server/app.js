30db16a8e44da815d260d822cc7391cbbbd88287
hasan sbeity <th.rider.clan@gmail.com>
2025-04-29 22:29:45 +0300

    updating the cors

--- server/app.js
+++ server/app.js
@@ -21,7 +21,7 @@ const PORT = process.env.PORT;
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: ["http://localhost:3000", "https://mila-react.onrender.com"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: [
       "Content-Type",
